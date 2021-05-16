d745b3e3fa3782e106227d3e7d19fb0aed6dfef2
kaveeshwa-iil <kaveeshwa.b@informaticsint.com>
2021-05-16 10:55:02 +0530

    Updates

---
+++ precache-manifest.81815fdd552c89963d14d738c46ce708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1580b00b2231e36d57ad85f5abd1b21",
+    "url": "/reviews/index.html"
+  },
+  {
+    "revision": "25c7359c3dea2073270f",
+    "url": "/reviews/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c7359c3dea2073270f",
+    "url": "/reviews/static/js/main.d07708d4.chunk.js"
+  },
+  {
+    "revision": "5496dc53684f16f6acfe",
+    "url": "/reviews/static/js/runtime-main.51a67177.js"
+  }
+]);
